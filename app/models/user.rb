04dd65a03b9f30484c49609ae2bd89aaefac88ac
Tasneem Kabir <tzkabir@gmail.com>
2012-12-30 20:04:38 +0000

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,9 @@
 class User < ActiveRecord::Base
 	attr_accessible(:email, :name, :password, :password_confirmation)
 	has_secure_password
+	
 	before_save { |user| user.email = email.downcase }
+	before_save :create_remember_token
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates(:name,
@@ -31,5 +33,11 @@ class User < ActiveRecord::Base
 	validates(:password_confirmation,
 					presence: true
 			)
-	
+
+	private
+
+		def create_remember_token
+			self.remember_token = SecureRandom.urlsafe_base64
+		end
+		
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -14,6 +14,14 @@ describe "User pages" do
 			it "should not create a user" do
 				expect { click_button submit }.not_to change(User, :count)
 			end
+
+			describe "after submission" do
+				before { click_button submit }
+
+				it { should have_selector('title', text: 'Sign up') }
+				it { should have_content('error') }
+			end
+				
 		end
 
 		describe "with valid information" do
@@ -22,6 +30,20 @@ describe "User pages" do
 				fill_in "Email", 		with: "user@example.com"
 				fill_in "Password", 	with: "foobar"
 				fill_in "Confirmation",	with: "foobar"
+			end
+
+			describe "after saving the user" do
+				before { click_button submit }
+				let(:user) { User.find_by_email('user@example.com') }
+
+				it { should have_selector('title', text: user.name) }
+				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+				it { should have_link('Sign out') }
+
+				describe "followed by signout" do
+					before { click_link "Sign out" }
+					it { should have_link('Sign in') }
+				end
 			end
 
 			it "should create a user" do
